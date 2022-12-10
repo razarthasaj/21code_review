69866b224827854f0070e164b2dbd6d6e475f420
razarthasaj <razarthasaj@gmail.com>
2022-12-10 19:26:56 +0000

    Added two functions to run via pytest

---
+++ 21_code_review.py
@@ -0,0 +1,30 @@
+/***In a language of your choice, using all best practice principles you’re aware of, could you please provide code that reads a list of 10 numbers ranging between 1 to 100.
+Assuming there are 4 duplicates in the list of numbers, the output should remove the duplicates and sort the remaining numbers in descending order. Please state any assumptions you’ve made***/
+
+test_list = [1, 5, 3, 6, 3, 5, 6, 1,.......]
+
+def test_read_list_of_ten_number(test_list):
+
+    # remove duplicates and add to the new empty list
+    # print 10 numbers from the list
+    
+    ten_list[]
+
+	for x in test_list:
+
+    	if test_list.count(x) > 1:
+    		if len(ten_list) != 10:
+    			append.ten_list(x)
+
+    return ten_list
+
+def test_remove_duplicates_from_the_lis(test_list):
+	# remove duplicates and display assending order [1, 2, 3, 4]
+
+	for x in test_list:
+
+    	if test_list.count(x) > 1:
+        	test_list.remove(x)
+
+        test_list.sort()	
+		print(ints_list)
---
+++ README.md
@@ -0,0 +1,5 @@
+Install: python3
+
+RUN: pip install pytest
+
+RUN: pytest 21_code_review.py
